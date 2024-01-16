f0d90a043225cd9aec46c425bf91a3d23a43f0e8
Ivaskiv <viraivaskiv@gmail.com>
2024-01-16 23:15:02 +0200

    upd css

--- src/components/friendListItem/FriendListItem.jsx
+++ src/components/friendListItem/FriendListItem.jsx
@@ -1,13 +1,11 @@
 import css from './friend-list-item.module.css';
 export default function FriendListItem({ avatar, name, isOnline }) {
+  const status = [`${css.marker} ${isOnline ? css.on : css.off}`];
   return (
     <div className={css.item}>
       <img className={css.avatar} src={avatar} alt="Avatar" width="48" />
-      <p className={css.name}>Friend name: {name}</p>
-      <p className={css.status}>Friend status: {isOnline}</p>
-      <span className={isOnline ? css.statusOnline : css.statusOffline}>
-        {isOnline ? 'Online' : 'Offline'}
-      </span>
+      <p className={css.name}>{name}</p>
+      <span className={status.join('')}></span>
     </div>
   );
 }

74b6d11b733d4c1e9dcb55811a76e35e52330168
Ivaskiv <viraivaskiv@gmail.com>
2024-01-16 23:56:33 +0200

    upd status.css

--- src/components/friendListItem/FriendListItem.jsx
+++ src/components/friendListItem/FriendListItem.jsx
@@ -1,10 +1,9 @@
 import css from './friend-list-item.module.css';
-export default function FriendListItem({ avatar, name, isOnline }) {
+export default function FriendListItem({ avatar, name }) {
   return (
     <div className={css.item}>
       <img className={css.avatar} src={avatar} alt="Avatar" width="48" />
       <p className={css.name}>{name}</p>
-      <span className={isOnline ? css.on : css.off}></span>
     </div>
   );
 }
